bdf9b06081e3478d31215c639599abcf53b6f36d
Mr. VannyCon <ivan.contrevida4@gmail.com>
2024-05-24 12:27:15 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EnviromentSustainability.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
